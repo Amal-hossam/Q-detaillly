7dd377c9cfee9f9397e2efdfcfd27de724156fdb
Amal <Amallhossamy@gmail.com>
2018-11-14 22:41:32 +0200

    updates

---
+++ js/plugin.js
@@ -0,0 +1,20 @@
+/*$(document).ready(function () {
+    $('.collapse.in').prev('.panel-heading').addClass('active');
+    $('#accordion, #bs-collapse')
+        .on('show.bs.collapse', function (a) {
+            $(a.target).prev('.panel-heading').addClass('active');
+        })
+        .on('hide.bs.collapse', function (a) {
+            $(a.target).prev('.panel-heading').removeClass('active');
+        });
+});
+*/
+
+$(function () {
+    $('a[href*=#]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
